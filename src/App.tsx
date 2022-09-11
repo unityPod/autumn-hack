814500cf54e210b7fb3202c5df97a4d8915a3c3c
Josh D <josh.date@techguilds.com>
2022-09-11 00:42:39 -0400

    added dropdown for registration and installed react forms and headless ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Registration from "./components/Registration/Registration";
+import Advisor from "./components/Registration/views/Advisor";
+import JobHunter from "./components/Registration/views/JobHunter";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/registration" element={<Registration />} />
+      <Route path="/job-hunter" element={<JobHunter />} />
+      <Route path="/advisor" element={<Advisor />} />
     </Routes>
   );
 };
--- src/components/Registration/views/Selection.tsx
+++ src/components/Registration/views/Selection.tsx
@@ -1,5 +1,23 @@
-const Selection = () => {
-  return <div>Selection</div>;
+import { Link } from "react-router-dom";
+
+interface ISelection {
+  handleClose: (openState?: boolean) => void;
+}
+
+const Selection = (props: ISelection) => {
+  const { handleClose } = props;
+
+  return (
+    <div>
+      <h2>Are You An Advisor or A Job Hunter?</h2>
+      <Link to="/job-hunter" onClick={() => handleClose(false)}>
+        Job Hunter
+      </Link>
+      <Link to="/advisor" onClick={() => handleClose(false)}>
+        Advisor
+      </Link>
+    </div>
+  );
 };
 
 export default Selection;
