76eb1c49ec733880e8ef079680893f4c395f507f
Josh D <josh.date@techguilds.com>
2022-09-11 19:54:32 -0400

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
+
 import Calendar from "./components/Calendar";
 import Registration from "./components/Registration/Registration";
-import Advisor from "./components/Registration/views/Advisor";
-import JobHunter from "./components/Registration/views/JobHunter";
 import SelectAdvisor from "./components/SelectAdvisor";
+import JobHunter from "./components/Registration/views/JobHunter";
+import Advisor from "./components/Registration/views/Advisor";
+import Login from "./components/Login/login";
 
 const App = () => {
   return (
