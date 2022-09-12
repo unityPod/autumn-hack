1c335099f744df4a55dfe03e2b63fe5f4e48c08e
unityPod <112590118+unityPod@users.noreply.github.com>
2022-09-11 18:09:20 -0700

    Update email and password sign up procedure with firebase

--- src/components/Registration/views/Advisor/Advisor.tsx
+++ src/components/Registration/views/Advisor/Advisor.tsx
@@ -1,21 +1,46 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "../Selection.module.css";
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import config from "../../../../config";
 
 type Inputs = {
   username: string;
   email: string;
+  password: string; 
   location: string;
   biography: string;
   role: string;
 };
 
 const Advisor = () => {
+  const auth = getAuth(config);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  function signUp(){
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        alert("The user was successfully created")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        // ..
+        alert(errorCode);
+      });
+  }
 
   return (
     <div className={styles["container"]}>
@@ -61,12 +86,35 @@ const Advisor = () => {
             pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
           })}
           id="email"
+          type={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         {errors.email?.type === "required" && (
           <span className="mb-4 text-error">Your Email Is Required</span>
         )}
         {errors.email?.type === "pattern" && (
           <span className="mb-4 text-error">Please Enter A Valid Email</span>
+        )}
+                <label htmlFor="password" className={styles["label-text"]}>
+          Password
+        </label>
+        <input
+          placeholder="Enter Your Password"
+          className={styles["input"]}
+          {...register("password", {
+            required: "Please enter your password",
+            /* eslint-disable */
+            pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          })}
+          id="password"
+          type={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errors.password?.type === "required" && (
+          <span className="mb-4 text-error">Your Password Is Required</span>
+        )}
+        {errors.password?.type === "pattern" && (
+          <span className="mb-4 text-error">Please Enter A Valid Password</span>
         )}
 
         <label className={styles["label-text"]} htmlFor="location">
@@ -125,7 +173,7 @@ const Advisor = () => {
           className={styles["input"]}
           readOnly
         />
-        <button className={styles["button"]} type="submit">
+        <button className={styles["button"]} type="submit" onClick={signUp}>
           Submit
         </button>
       </form>
